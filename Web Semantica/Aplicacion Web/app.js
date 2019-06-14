98e6f7e3f48d839b598af75b8375dfb43c780538
aMinguezg <44700540+aMinguezg@users.noreply.github.com>
2019-06-14 02:27:26 +0200

    Aplicacion acabada

--- Web Semantica/Aplicacion Web/app.js
+++ Web Semantica/Aplicacion Web/app.js
@@ -2,17 +2,25 @@ let express = require('express');
 let app = express();
 let swig  = require('swig');
 let bodyParser = require('body-parser');
+let methodOverride = require("method-override");
+
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
+
+app.use(methodOverride());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Authorization,Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.static('public'));
 
 require("./routes/rdatos.js")(app,swig);
 
 
-
-
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Listening on port 3000!');
 });
--- Web Semantica/Aplicacion Web/routes/rdatos.js
+++ Web Semantica/Aplicacion Web/routes/rdatos.js
@@ -1,9 +1,9 @@
 let request = require("request");
 let parser = require('xml2js').parseString;
 
-module.exports = function(app, swig) {
+module.exports = function (app, swig) {
 
-	app.get('/', function (req, res) {
+    app.get('/', function (req, res) {
 
 
         let resultado = swig.renderFile('views/index.html', {
@@ -11,79 +11,60 @@ module.exports = function(app, swig) {
         res.send(resultado)
     });
 
-
-    app.get('/:id', function (req, res) {
-
-        let options = {
-            method: 'GET',
-            url: 'https://query.wikidata.org/sparql',
-            qs: { query: 'SELECT * {wd:'+req.params.id+' p:P1082 ?nodo . ?nodo ps:P1082 ?poblacion . ?nodo pq:P585 ?momento}' },
-            headers:
-            {
-                'Postman-Token': '4172e2b1-f59f-44c5-8550-86133584d1ab',
-                'cache-control': 'no-cache'
-            }
-        };
-    
-        request(options, function (error, response, body) {
-            if (error) throw new Error(error);
-            let poblaciones = [];
-            let years = [];
-            let i;
-            parser(body, function (err, result) {
-                let numResultados = Object.keys(result['sparql']['results'][0]['result']).length;
-                for (i = 0; i < numResultados; i++){
-                    poblaciones.push(result['sparql']['results'][0]['result'][i]['binding'][1]['literal'][0]['_']);
-                    let tempYear = result['sparql']['results'][0]['result'][i]['binding'][2]['literal'][0]['_']
-                    let year = tempYear.substring(0,4);
-                    years.push(year)
-                }
-                console.log(poblaciones );
-                console.log(years);
-                let resultado = swig.renderFile('views/index.html', {
-                    poblaciones: poblaciones
-                });
-                
-                res.send(resultado)
-            });       
-        });    
-    });
-
     app.post('/', function (req, res) {
 
         let options = {
             method: 'GET',
             url: 'https://query.wikidata.org/sparql',
-            qs: { query: 'SELECT * {wd:'+req.body.ciudad+' p:P1082 ?nodo . ?nodo ps:P1082 ?poblacion . ?nodo pq:P585 ?momento}' },
+            qs: { query: 'SELECT * {wd:' + req.body.ciudad + ' p:P1082 ?nodo . ?nodo ps:P1082 ?poblacion . ?nodo pq:P585 ?momento}' },
             headers:
             {
                 'Postman-Token': '4172e2b1-f59f-44c5-8550-86133584d1ab',
                 'cache-control': 'no-cache'
             }
         };
-    
+
         request(options, function (error, response, body) {
             if (error) throw new Error(error);
             let poblaciones = [];
             let years = [];
+            let resultados = {};
             let i;
             parser(body, function (err, result) {
-                let numResultados = Object.keys(result['sparql']['results'][0]['result']).length;
-                for (i = 0; i < numResultados; i++){
-                    poblaciones.push(result['sparql']['results'][0]['result'][i]['binding'][1]['literal'][0]['_']);
-                    let tempYear = result['sparql']['results'][0]['result'][i]['binding'][2]['literal'][0]['_']
-                    let year = tempYear.substring(0,4);
-                    years.push(year)
+                if (result['sparql']['results'][0]['result'] == undefined) {
+
+                    let resultado = swig.renderFile('views/data.html', {
+                        poblaciones: poblaciones,
+                        error: 'Error, el id introducido no es correcto',
+                        years: years
+                    });
+                    res.send(resultado);
                 }
-                console.log(poblaciones );
-                console.log(years);
-                let resultado = swig.renderFile('views/data.html', {
-                    poblaciones: poblaciones
-                });
-                
-                res.send(resultado)
-            });       
-        });    
+                else {
+
+                    let numResultados = Object.keys(result['sparql']['results'][0]['result']).length;
+                    for (i = 0; i < numResultados; i++) {
+                        let tempYear = result['sparql']['results'][0]['result'][i]['binding'][2]['literal'][0]['_']
+                        let year = tempYear.substring(0, 4);
+                        resultados[year] = result['sparql']['results'][0]['result'][i]['binding'][1]['literal'][0]['_']
+
+                    }
+                    for (let key in resultados) {
+                        years.push(key);
+                        poblaciones.push(resultados[key]);
+                    }
+
+                    let resultado = swig.renderFile('views/data.html', {
+                        poblaciones: poblaciones,
+                        ciudad: req.body.ciudad,
+                        years: years
+                    });
+
+                    res.send(resultado)
+                }
+
+            });
+        });
     });
 
 };
